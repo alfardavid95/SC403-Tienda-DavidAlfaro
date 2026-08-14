06fd33c13c1d28e81d1348c890ed9d9980d34516
David Alfaro Cordero <alfardavid01@gmail.com>
2026-08-13 22:31:49 -0600

    feat: agregar pagina publica de informacion del proyecto

---
+++ src/main/java/com/tienda/controller/InfoProyectoController.java
@@ -0,0 +1,15 @@
+package com.tienda.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/consultas")
+public class InfoProyectoController {
+
+    @GetMapping("/acerca")
+    public String acerca() {
+        return "consultas/acerca";
+    }
+}
